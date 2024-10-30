cedc1be6bb344f44563c3ebe959e9c814e0d109c
Пользователь <@WE_RE>
2024-10-30 16:43:52 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Лаба3._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private SolidColorBrush currentColor = Brushes.Black;
+        private double currentBrushSize = 5;
+        private bool isDrawing = false;
+        private Point previousPoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ColorComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string colorName = selectedItem.Tag.ToString();
+                currentColor = (SolidColorBrush)new BrushConverter().ConvertFromString(colorName);
+            }
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            currentBrushSize = e.NewValue;
+        }
+
+        private void Mode_Checked(object sender, RoutedEventArgs e)
+        {
+            // Здесь можно реализовать логику переключения режимов
+        }
+
+        private void DrawingCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (DrawMode.IsChecked == true)
+            {
+                isDrawing = true;
+                previousPoint = e.GetPosition(DrawingCanvas);
+            }
+        }
+
+        private void DrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point currentPoint = e.GetPosition(DrawingCanvas);
+                Line line = new Line
+                {
+                    Stroke = currentColor,
+                    StrokeThickness = currentBrushSize,
+                    X1 = previousPoint.X,
+                    Y1 = previousPoint.Y,
+                    X2 = currentPoint.X,
+                    Y2 = currentPoint.Y
+                };
+
+                DrawingCanvas.Children.Add(line);
+                previousPoint = currentPoint;
+            }
+        }
+
+        private void DrawingCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isDrawing)
+            {
+                isDrawing = false;
+            }
+        }
+    }
+}
